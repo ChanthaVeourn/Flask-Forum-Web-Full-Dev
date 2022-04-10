fe45acb953784e27d12cf74b5a5d0a164546fd79
tha <tha@gmail.com>
2022-04-10 14:56:36 +0700

    full stack web dev

--- app/routes/reply_view.py
+++ app/routes/reply_view.py
@@ -6,18 +6,15 @@ from flask_login import current_user, login_required
 from app.form import ReplyForm
 reply_blueprint = Blueprint('reply', __name__,template_folder="app/templates/",url_prefix="/reply")
 
-@reply_blueprint.route('/<slush>/', methods=['GET', 'POST'])
+@reply_blueprint.route('/<slush>/', methods=['GET'])
 def create_reply(slush):
-
-    if request.method == 'POST':
-        pass
 
     current_forum = Forum.query.filter(Forum.slush == slush).one()
     if not current_forum:
         abort(404)
     
-    #all_replies = current_forum
+    all_replies = Reply.query.filter(Reply.forum_id == current_forum.id).all()
 
-    return render_template("all_replies.html", replies = all_replies)
+    return render_template("all_replies.html", replies = all_replies,forum=current_forum)
 
 
